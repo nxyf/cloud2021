93d743414fdf1c0ce5e0780c8aae3a5e36470c92
niutian <123@163.com>
2021-01-19 21:52:53 +0800

    gateway

---
+++ cloud-gateway-gateway9527/src/main/java/com/nxyf/springcloud/GateWayApplication.java
@@ -0,0 +1,19 @@
+package com.nxyf.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @program: cloud2021
+ * @description:
+ * @author: myj
+ * @create: 2021-01-19 20:13
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+}
---
+++ cloud-gateway-gateway9527/src/main/java/com/nxyf/springcloud/filter/MyLogGatewayFilter.java
@@ -0,0 +1,36 @@
+package com.nxyf.springcloud.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.StringUtils;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @program: cloud2021
+ * @description:自定义全局过滤器
+ * @author: myj
+ * @create: 2021-01-19 21:36
+ */
+//@Component
+@Slf4j
+public class MyLogGatewayFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+        if (StringUtils.isEmpty(uname)) {
+            log.info("用户名为空");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}

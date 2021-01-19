d3bbdad1ab5b0910e0ae66b52b4873eb587e25c3
niutian <123@163.com>
2021-01-19 22:37:38 +0800

    config

---
+++ cloud-config-center/src/main/java/com/nxyf/springcloud/ConfigApplication.java
@@ -0,0 +1,19 @@
+package com.nxyf.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @program: cloud2021
+ * @description:
+ * @author: myj
+ * @create: 2021-01-19 22:21
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}

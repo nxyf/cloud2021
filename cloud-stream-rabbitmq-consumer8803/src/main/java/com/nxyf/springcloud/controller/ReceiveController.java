d87ae7a7abf37a2ef3fbd3ba0469ae2ac92f5646
niutian <123@163.com>
2021-01-23 09:38:09 +0800

    spring cloud stream

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/nxyf/springcloud/controller/ReceiveController.java
@@ -0,0 +1,29 @@
+package com.nxyf.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+/**
+ * @program: springcloud
+ * @description:
+ * @author: myj
+ * @create: 2021-01-23 08:44
+ */
+@Component
+@EnableBinding(Sink.class)
+@Slf4j
+public class ReceiveController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message){
+       log.info( "消费者2号，接收到的消息=======  " + message.getPayload() + "=========端口号：" + serverPort);
+    }
+}
